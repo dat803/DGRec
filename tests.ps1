577ad7e025dc1a7e9fcaa5eadc1a1b82bc1ea8ef
Lucas2k <lucasmartin2k@gmail.com>
2025-04-08 09:00:02 +0200

    args parser takes string for metric list

--- tests.ps1
+++ tests.ps1
@@ -1,5 +1,2 @@
-python main.py --kernel poly;
-python main.py --kernel tanh;
-python main.py --dataset Beauty;
-python main.py --dataset Beauty --kernel poly;
-python main.py --dataset Beauty --kernel tanh;
+python main.py --epoch 1 --dataset Beauty --metrics "recall,hit_ratio,coverage"
+# python main.py --epoch 1 --dataset Beauty --gpu -1 --metrics "recall,hit_ratio,coverage,discount_coverage";
