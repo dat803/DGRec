f7b973428c58cd0796dadf04b18efbbe1cfeb2ca
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2025-05-05 14:10:31 +0200

    Fix too long pathname

--- taobao.sh
+++ taobao.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
-#SBATCH --job-name=taobao.out
-#SBATCH --output=taobao.out
-#SBATCH --error=taobao.err
+#SBATCH --job-name=taobao
+#SBATCH --output=taobao_%j.out
+#SBATCH --error=taobao_%j.err
 #SBATCH --mem=24G
 #SBATCH --cpus-per-task=15
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=seblar21@student.aau.dk
+#SBATCH --array=0-647%8    # TODO
 
-# popularity: True and False
-# kernel: gaussian, poly, tanh 
-# learning rate: 0.1, 0.05, 0.01
-# sigma: only for gaussian kernel 0.5, 1.0, 2.0
-# gamma: only for gaussian kernel 1.0, 2.0, 3.0
-# embed_size: 32,64
+datasets=("Beauty" "TaoBao")
+embed_sizes=(32 64)
+kernels=("gaussian" "poly" "tanh")
+popularities=(False True)
+learning_rates=(0.05 0.1 0.01)
+sigmas=(1.0 0.5 2.0)
+gammas=(2.0 1.0 3.0)
 
-# testing single values 
-# DILAD_beta: 0.004 for TaoBao and 0.005 for Beauty
-# k_list: 1,5,10,50,100,300
-# DCC_alpha: 0.008
-# FDCC_alpha: 0.008
-
-singularity exec \
-    --nv /ceph/project/dat803-25/dgl_25.03-py3.sif python "/ceph/project/dat803-25/DGRec/main.py" \
-    --metrics recall,hit_ratio,coverage,IUD,DILAD,ILAD,DCC,FDCC \ 
-    --DCC_alpha 0.008
-    --FDCC_alpha 0.008
-    --DILAD_beta 0.004
-    --k_list 1,5,10,50,100,300
+for dataset in "${datasets[@]}"; do
+  for embed_size in "${embed_sizes[@]}"; do
+    for kernel in "${kernels[@]}"; do
+      for popularity in "${popularities[@]}"; do
+        for learning_rate in "${learning_rates[@]}"; do
+          for sigma in "${sigmas[@]}"; do
+            for gamma in "${gammas[@]}"; do
+              singularity exec --nv /ceph/project/dat803-25/dgl_25.03-py3.sif \
+              python "/ceph/project/dat803-25/DGRec/main.py" \
+                --metrics recall,hit_ratio,coverage,IUD,DILAD,ILAD,DCC,FDCC \
+                --dataset "$dataset" \
+                --kernel "$kernel" \
+                --embed_size "$embed_size" \
+                --popularity "$popularity" \
+                --lr "$learning_rate" \
+                --sigma "$sigma" \
+                --gamma "$gamma" \
+                --DCC_alpha 0.008 \
+                --FDCC_alpha 0.008 \
+                --DILAD_beta $( [[ "$dataset" == "TaoBao" ]] && echo 0.004 || echo 0.005 ) \
+                --k_list 1,5,10,50,100,300
+            done
+          done
+        done
+      done
+    done
+  done
+done
--- utils/utils.py
+++ utils/utils.py
@@ -59,8 +59,10 @@ def config(args):
     if (args.model_path):
         path = os.path.basename(args.model_path).split('/')[-1]
     else:
-        path = f"{args.dataset}_model_{args.model}_lr_{args.lr}_embed_size_{args.embed_size}_batch_size_{args.batch_size}_weight_decay_{args.weight_decay}_layers_{args.layers}_neg_number_{args.neg_number}_seed_{args.seed}_k_{args.k}_sigma_{args.sigma}_gamma_{args.gamma}_coef0_{args.coef0}_degree_{args.degree}_beta_class_{args.beta_class}_kernel_function_{args.kernel}_submodular_function_{args.submodular_selection_option}_popularity_{args.popularity}_DCC_alpha_{args.DCC_alpha}_FDCC_alpha_{args.FDCC_alpha}_DILAD_beta_{args.DILAD_beta}"
-    
+        #path = f"{args.dataset}_model_{args.model}_lr_{args.lr}_embed_size_{args.embed_size}_batch_size_{args.batch_size}_weight_decay_{args.weight_decay}_layers_{args.layers}_neg_number_{args.neg_number}_seed_{args.seed}_k_{args.k}_sigma_{args.sigma}_gamma_{args.gamma}_coef0_{args.coef0}_degree_{args.degree}_beta_class_{args.beta_class}_kernel_function_{args.kernel}_submodular_function_{args.submodular_selection_option}_popularity_{args.popularity}_DCC_alpha_{args.DCC_alpha}_FDCC_alpha_{args.FDCC_alpha}_DILAD_beta_{args.DILAD_beta}"
+        path = f"{args.dataset}_m_{args.model}_lr_{args.lr}_es_{args.embed_size}_bs_{args.batch_size}_wd_{args.weight_decay}_l_{args.layers}_nn_{args.neg_number}_s_{args.seed}_k_{args.k}_sigma_{args.sigma}_gamma_{args.gamma}_coef0_{args.coef0}_degree_{args.degree}_bc_{args.beta_class}_kf_{args.kernel}_sf_{args.submodular_selection_option}_p_{args.popularity}_DCCa_{args.DCC_alpha}_FDCC_a_{args.FDCC_alpha}_DILAD_b_{args.DILAD_beta}"
+
+
     if (args.output and args.model_name):
         os.makedirs(args.output, exist_ok=True)
         output_log_file_path = args.output + "/" + args.model_name + '.log'
